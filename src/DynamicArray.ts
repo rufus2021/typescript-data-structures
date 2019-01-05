4c1960e3dd608e28e67b01a78b54a1322332cb7c
Jeff Morin <rufus2021@gmail.com>
2019-01-05 12:33:12 -0500

    implement the bulk of array

---
+++ src/DynamicArray.ts
@@ -0,0 +1,163 @@
+interface DynamicArrayIface {
+  getCapacity(): number;
+  getSize(): number;
+  isEmpty(): boolean;
+  append(val: number): void;
+  prepend(val: number): void;
+  set(i: number, val: number): void;
+  getAt(i: number): number; // value at given index
+  deleteAt(i: number): void;
+  // insertAt(i: number, val: number): void;
+  find(val: number): number; // index value or -1 if not found
+  pop(): number;
+  remove(val: number): void;
+}
+
+export default class DynamicArray implements DynamicArrayIface {
+  private dynamicArray: number[];
+  private capacity: number;
+
+  constructor() {
+    this.dynamicArray = [];
+    this.capacity = 4;
+  }
+
+  getCapacity(): number {
+    return this.capacity;
+  }
+
+  getSize(): number {
+    return this.dynamicArray.length;
+  }
+
+  isEmpty(): boolean {
+    return this.dynamicArray.length === 0;
+  }
+
+  // O(1) if not at capacity
+  // O(n) if at capacity
+  append(val: number): void {
+    const size = this.getSize();
+    if (size === this.capacity) {
+      // for learning purposes since arrays are dynamic in JavaScript
+      // 1. create a new array
+      // 2. copy contents to new array
+      // 3. add new value to the end
+      // 4. replace dynamicArray with replacement
+      // 5. update capacity
+      const replacement = [];
+      for (let i = 0; i < size; i++) {
+        replacement[i] = this.dynamicArray[i];
+      }
+      replacement[size] = val;
+      this.dynamicArray = replacement;
+      this.capacity *= 2;
+    }
+
+    this.dynamicArray[size] = val;
+  }
+
+  // O(n)
+  prepend(val: number): void {
+    let size = this.getSize();
+    if (size === this.capacity) {
+      // for learning purposes since arrays are dynamic in JavaScript
+      // 1. create a new array
+      // 2. copy contents to new array (moving each item forward 1 place)
+      // 3. add new value to the beginning
+      // 4. replace dynamicArray with replacement
+      // 5. update capacity
+      const replacement = [];
+      for (let i = 0; i < size; i++) {
+        replacement[i + 1] = this.dynamicArray[i];
+      }
+      replacement[0] = val;
+      this.dynamicArray = replacement;
+      this.capacity *= 2;
+    } else {
+      while (size > 0) {
+        // 1. start at the end, moving each i - 1 forward 1 place
+        // 2. add to the beginning
+        this.dynamicArray[size] = this.dynamicArray[size - 1];
+        size--;
+      }
+      this.dynamicArray[0] = val;
+    }
+  }
+
+  set(i: number, val: number): void {
+    if (i >= this.capacity) {
+      throw new Error('Out of range');
+    }
+
+    this.dynamicArray[i] = val;
+  }
+
+  // return value at index
+  // O(1)
+  getAt(i: number) {
+    const size = this.getSize();
+    if (0 === size) {
+      throw new Error('Empty array');
+    }
+
+    if (i >= size) {
+      throw new Error('Out of range');
+    }
+    return this.dynamicArray[i];
+  }
+
+  // O(1) if end
+  // O(n) anywhere else
+  /**
+   *
+   * NOT COMPLETE
+   */
+  deleteAt(i: number): void {
+    const size = this.getSize();
+    if (0 === size) {
+      throw new Error('Empty array');
+    }
+
+    if (i >= size) {
+      throw new Error('Out of range');
+    }
+
+    this.dynamicArray[i] = null;
+
+    // if end pop last element
+    // if not, move all values at i + 1 up 1 value
+    // pop last value to set correct length
+  }
+
+  // O(n)
+  find(val: number): number {
+    const size = this.getSize();
+    for (let i = 0; i < size; i++) {
+      if (this.dynamicArray[i] === val) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  pop(): number {
+    const size = this.getSize()
+    if (0 === size) {
+      throw new Error('Empty array');
+    }
+    // cheating w/ a built in
+    // delete sets the value to undefined and
+    // won't update the array length
+    return this.dynamicArray.pop();
+  }
+
+  remove(val: number): void {
+    const size = this.getSize();
+    for (let i = 0; i < size; i++) {
+
+    }
+
+    throw new Error('Value not found');
+  }
+}
---
+++ test/DynamicArray.spec.js
@@ -0,0 +1,185 @@
+const { default: DynamicArray } = require('../src/DynamicArray');
+const { assert, expect } = require('chai');
+
+describe('Dynamic Array', function () {
+  let dArray;
+
+  beforeEach(() => {
+    dArray = new DynamicArray();
+  });
+
+  it('can be instantiated', () => {
+    assert.isObject(dArray);
+  });
+
+  it('creates an array', () => {
+    assert.isArray(dArray.dynamicArray);
+  });
+
+  describe('methods', () => {
+
+    it('getCapacity', () => {
+      assert.equal(dArray.getCapacity(), 4);
+    });
+
+    it('getSize', () => {
+      assert.equal(dArray.getSize(), 0);
+    });
+
+    it('isEmpty truthy', () => {
+      assert.isTrue(dArray.isEmpty());
+    });
+
+    it('isEmpty falsey', () => {
+      dArray.append(1);
+      assert.isFalse(dArray.isEmpty());
+    });
+
+    it('getAt (empty array error)', () => {
+      expect(() => dArray.getAt(5)).to.throw('Empty array');
+    });
+
+    it('getAt (out of range error)', () => {
+      dArray.append(1);
+      expect(() => dArray.getAt(5)).to.throw('Out of range');
+    });
+
+    it('getAt (returns a value at a given index)', () => {
+      dArray.append(1);
+      dArray.append(2);
+      dArray.append(3);
+      assert.equal(dArray.getAt(0), 1);
+      assert.equal(dArray.getAt(1), 2);
+      assert.equal(dArray.getAt(2), 3);
+    });
+
+    it('set (out of range error)', () => {
+      expect(() => dArray.set(5, 5)).to.throw('Out of range');
+    });
+
+    it('set (updates a value at a given index', () => {
+      dArray.append(1);
+      dArray.append(2);
+      dArray.append(3);
+      dArray.append(4);
+      assert.equal(dArray.getAt(0), 1);
+      assert.equal(dArray.getAt(1), 2);
+      assert.equal(dArray.getAt(2), 3);
+      assert.equal(dArray.getAt(3), 4);
+      dArray.set(0, 2);
+      dArray.set(1, 3);
+      dArray.set(2, 4);
+      dArray.set(3, 5);
+      assert.equal(dArray.getAt(0), 2);
+      assert.equal(dArray.getAt(1), 3);
+      assert.equal(dArray.getAt(2), 4);
+      assert.equal(dArray.getAt(3), 5);
+    });
+
+    it('append - (with room) - adds to the end', () => {
+      assert.equal(dArray.getSize(), 0);
+      dArray.append(1);
+      assert.equal(dArray.getAt(0), 1);
+      dArray.append(2);
+      assert.equal(dArray.getAt(1), 2);
+    });
+
+    it('append (at capacity) - copies array contents and updates capacity', () => {
+      dArray.append(1);
+      dArray.append(2);
+      dArray.append(3);
+      dArray.append(4);
+      assert.equal(dArray.getAt(3), 4);
+      assert.equal(dArray.getCapacity(), 4);
+      dArray.append(5);
+      assert.equal(dArray.getAt(4), 5);
+      assert.equal(dArray.getCapacity(), 8);
+    });
+
+    it('prepend - (with room) - adds to the beginnning', () => {
+      assert.equal(dArray.getSize(), 0);
+      dArray.prepend(2);
+      assert.equal(dArray.getAt(0), 2);
+      dArray.prepend(1);
+      assert.equal(dArray.getAt(0), 1);
+      assert.equal(dArray.getAt(1), 2);
+    });
+
+    it('prepend (at capacity) - copies array contents and updates capacity', () => {
+      dArray.prepend(4);
+      dArray.prepend(3);
+      dArray.prepend(2);
+      dArray.prepend(1);
+      assert.equal(dArray.getAt(0), 1);
+      assert.equal(dArray.getCapacity(), 4);
+      dArray.prepend(0);
+      assert.equal(dArray.getAt(0), 0);
+      assert.equal(dArray.getCapacity(), 8);
+    });
+
+    it('deleteAt (empty array error)', () => {
+      expect(() => dArray.deleteAt(5)).to.throw('Empty array');
+    });
+
+    it('deleteAt (out of range error)', () => {
+      dArray.append(1);
+      expect(() => dArray.deleteAt(5)).to.throw('Out of range');
+    });
+
+    it('deleteAt (deletes a value at a given index)', () => {
+      dArray.append(1);
+      dArray.append(2);
+      dArray.append(3);
+      dArray.append(4);
+      assert.equal(dArray.getAt(0), 1);
+      assert.equal(dArray.getAt(1), 2);
+      assert.equal(dArray.getAt(2), 3);
+      assert.equal(dArray.getAt(3), 4);
+      dArray.deleteAt(0);
+      dArray.deleteAt(1);
+      dArray.deleteAt(2);
+      dArray.deleteAt(3);
+      assert.isNull(dArray.getAt(0));
+      assert.isNull(dArray.getAt(1));
+      assert.isNull(dArray.getAt(2));
+      assert.isNull(dArray.getAt(3));
+    });
+
+    it('find (returns the index position of a given value', () => {
+      dArray.append(3);
+      dArray.append(4);
+      dArray.append(5);
+      assert.equal(dArray.find(5), 2);
+    });
+
+    it('find (returns -1 if value isn\'t found', () => {
+      assert.equal(dArray.find(5), -1);
+    });
+
+    it('pop (empty array error)', () => {
+      expect(() => dArray.pop()).to.throw('Empty array');
+    });
+
+    it('pop (returns the last value)', () => {
+      dArray.append(1);
+      dArray.append(2);
+      dArray.append(3);
+      assert.equal(dArray.pop(), 3);
+      assert.equal(dArray.pop(), 2);
+      assert.equal(dArray.pop(), 1);
+    });
+
+    it('remove (value not found error)', () => {
+      expect(() => dArray.remove(3)).to.throw('Value not found');
+    });
+
+    it('remove (deletes the index of a given value)', () => {
+
+    });
+
+    // it('find', () => {
+
+    // });
+
+  });
+});
