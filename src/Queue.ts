d4d883665d7631a379aebfed1e23a72c1702c446
Jeff Morin <rufus2021@users.noreply.github.com>
2019-01-09 17:40:28 -0500

    Jsdoc (#11)
    Document Queue

--- src/Queue.ts
+++ src/Queue.ts
@@ -6,21 +6,30 @@ interface QueueContract {
   empty(): boolean;
 }
 
+/**
+ * A Queue implementation using a linked list
+ * All operations are constant time since the
+ * linked list tracks the tail so no traversing
+ * is needed to reach the last item (which is the first added)
+ */
 export default class Queue extends SList implements QueueContract {
   constructor() {
     super();
   }
 
+  // O(1)
   enqueue(val: number): void {
     this.pushBack(val);
   }
 
+  // O(1)
   dequeue(): number {
     const result = this.topFront();
     this.popFront();
     return result;
   }
 
+  // O(1)
   empty(): boolean {
     return this.getSize() === 0;
   }
