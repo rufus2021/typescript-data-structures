650d21621e7a232bf61b14bde34ab213afec4e94
Jeff Morin <rufus2021@gmail.com>
2019-04-25 17:41:41 -0400

    types file rename, import binary tree types

--- src/BinaryTree.ts
+++ src/BinaryTree.ts
@@ -1,13 +1,4 @@
-interface Node {
-  value: number
-  left: Node
-  right: Node
-}
-
-interface BinaryTreeIface {
-  insert(current: Node, value: number): void
-  find(current: Node, value: number): number | boolean
-}
+import { Node, BinaryTreeIface } from '../types';
 
 export default class BinaryTree implements BinaryTreeIface {
   root: Node;
